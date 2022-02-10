b6fffe3831b0158729e45a1c42d1ffe8156b60e5
Tani Cha <91624779+tanicha@users.noreply.github.com>
2022-02-10 17:48:06 +0000

    adds hide() to two form elements

--- index.php
+++ index.php
@@ -44,6 +44,14 @@
 <script>
     $("document").ready(function(){
         
+      //hide likes and eats
+      $('#pet_likes').hide();
+      $('#pet_eats').hide();
+
+        //on click of feels, likes is shown
+        $('#pet_feels').click(function(e){
+          $('#pet_likes').slideDown(200);
+        });
         
         $('#myForm').submit(function(e){
             e.preventDefault();//no need to submit as you'll be doing AJAX on this page
